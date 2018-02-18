78f7bbc7fe28b594d67c9fb3658f749f70fe0130
SoleSS <soless@pjatigorsk.ru>
2018-02-18 10:13:33 +0300

    JS fix rewrite

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,4 +1,5 @@
-$(document).on('click', '.tageditor-wrap .available-tags-wrap a.btn', function () {
+$(document).on('click', '.tageditor-wrap .available-tags-wrap a.btn', function (event) {
+  event.preventDefault();
   $(this).parent().parent().find('input.tag-editor-hidden-src').tagEditor('addTag', $(this).text());
   return false;
 });
